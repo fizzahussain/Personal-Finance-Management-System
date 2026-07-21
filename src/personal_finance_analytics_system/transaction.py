63c90bbaab8aaea00f51c4fbbdeca4a40c8aaa69
fizza-org <fizza@betacodes.pk>
2026-07-21 13:39:56 +0500

    feature/income

--- src/personal_finance_analytics_system/transaction.py
+++ src/personal_finance_analytics_system/transaction.py
@@ -26,10 +26,7 @@ class Transaction:
         if self.transaction_type == "income":
             return self.amount
 
-<<<<<<< Updated upstream
         return -self.amount
-=======
-        return -self.amount
-     
 
->>>>>>> Stashed changes
+    def print():
+        print("hi")
