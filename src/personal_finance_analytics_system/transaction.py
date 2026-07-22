ed9ce2128ff82e65cd88f7422948a00802f60bba
fizza-org <fizza@betacodes.pk>
2026-07-22 12:51:32 +0500

    add: transaction-manager fixed

--- src/personal_finance_analytics_system/transaction.py
+++ src/personal_finance_analytics_system/transaction.py
@@ -26,10 +26,4 @@ class Transaction:
         if self.transaction_type == "income":
             return self.amount
 
-<<<<<<< Updated upstream
         return -self.amount
-=======
-        return -self.amount
-     
-
->>>>>>> Stashed changes
