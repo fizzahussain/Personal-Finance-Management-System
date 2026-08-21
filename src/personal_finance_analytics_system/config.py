3bc574da52e5f4c61c098e70c72c447450ecefe6
Fizza Hussain <fizzashah0300@gmail.com>
2026-08-22 02:28:49 +0500

    config changes for vercel

--- src/personal_finance_analytics_system/config.py
+++ src/personal_finance_analytics_system/config.py
@@ -1,10 +1,6 @@
 from pathlib import Path
 
-PROJECT_ROOT = Path(__file__).resolve().parents[2]
-DATA_DIRECTORY = PROJECT_ROOT / "data"
-DATABASE_PATH = DATA_DIRECTORY / "transactions.db"
+DATA_DIRECTORY = Path("/tmp/personal_finance_data")
+DATA_DIRECTORY.mkdir(parents=True, exist_ok=True)
 
-DATA_DIRECTORY.mkdir(
-    parents=True,
-    exist_ok=True,
-)
+DATABASE_PATH = DATA_DIRECTORY / "transactions.db"
